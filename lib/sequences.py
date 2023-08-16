f7c867701a63e4e827e55f923197ce206a7c865a
michellemwangi01 <michellemwangim@gmail.com>
2023-08-16 08:30:52 +0300

    fibonacci sequence created

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+
+    fibonacci_arr = []
+    if length == 0:
+        print( fibonacci_arr)
+    elif length == 1:
+        fibonacci_arr.append(0)
+        print(fibonacci_arr)
+    elif length == 2:
+        fibonacci_arr = [0,1]
+        print(fibonacci_arr)
+    elif length > 2:
+        fibonacci_arr = [0,1]
+        for i in range(length-2):
+            fibonacci_arr.append(fibonacci_arr[-1] + fibonacci_arr[-2])
+        print(fibonacci_arr)
+
+
+
+print_fibonacci(10)
